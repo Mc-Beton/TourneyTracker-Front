61ff00b35accef0e42ed4e1e3d03014e4f811c74
Filberto <el.filberto@gmail.com>
2026-02-15 00:14:41 +0100

    Add standalone output for Docker

--- tt-front/next.config.js
+++ tt-front/next.config.js
@@ -12,6 +12,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   /* config options here */
 };
 
